38abde5790c9e38d3b530c925947781c4e16dc0e
Fabrizio Verde <signoredellarete@gmail.com>
2025-06-20 18:18:57 +0200

    create secret generic github-credentials

--- scripts/create_git_secret.sh
+++ scripts/create_git_secret.sh
@@ -1,5 +1,4 @@
 oc create secret generic github-credentials \
   --from-literal=user=<USERNAME_GITHUB> \
   --from-literal=accessToken=<YOUR_TOKEN> \
-  -n open-cluster-management \
-  --type=kubernetes.io/basic-auth
+  -n open-cluster-management
